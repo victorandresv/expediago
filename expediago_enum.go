58d5a117754002b833f63e44f3914c3da55b6289
agentcars <victor.vargas@agentcars.com>
2023-08-16 21:34:47 -0300

    Const enum file

---
+++ expediago_enum.go
@@ -0,0 +1,18 @@
+package expediago
+
+const (
+	SalesEnvironmentHotelOnly string = "hotel_only"
+	SalesChannelWebsite       string = "website"
+)
+
+const (
+	CurrencyUnitedStatesDollar string = "USD"
+)
+
+const (
+	CountryCodeUS string = "US"
+)
+
+const (
+	LanguageUnitedStatesEnglish string = "en-US"
+)
